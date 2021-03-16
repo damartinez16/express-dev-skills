9fa8f98b6ce77354688792aebeb4ae7aaa09cb3e
Danielle Martinez <daniellealexandria10@gmail.com>
2021-03-16 16:00:41 -0600

    initial commit

---
+++ routes/skills.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+// Require the controller for the skills resource
+var skillsCtrl = require('../controllers/skills');
+
+// All actual paths begin with "/skills"
+
+/* GET /skills     --- "index" action/functionality */
+router.get('/', skillsCtrl.index);
+// GET /skills/:id
+router.get('/:id', skillsCtrl.show);
+
+
+module.exports = router;
